b36f0dfca3a59560e1fe3d22465362d26b5213b7
René <renarutosoto@gmail.com>
2020-03-22 01:33:04 -0600

    Sericios completo

--- resotor/Microservicios/estado-cuenta/src/main/java/com/thincode/estadocuenta/configuration/Configuracion.java
+++ resotor/Microservicios/estado-cuenta/src/main/java/com/thincode/estadocuenta/configuration/Configuracion.java
@@ -1,11 +1,13 @@
 package com.thincode.estadocuenta.configuration;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableHystrixDashboard
 public class Configuracion {
 	
 	@Bean
