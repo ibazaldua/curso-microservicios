aa13ec8b5f12c47861180dff1b106a759d7b2eb9
Juan Diego Cruz Marquez <jcruzmz111296@gmail.com>
2020-03-23 13:13:54 -0600

    Rene realizo modificaciones desde mi repositorio

--- resotor/Expresiones/src/main/java/com/thincode/logic/Servicio.java
+++ resotor/Expresiones/src/main/java/com/thincode/logic/Servicio.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,24 +16,24 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import com.thincode.objects.Usuario;
 
 public class Servicio {
-	
+
 	JFileChooser selectorArchivos;
 	FileNameExtensionFilter filtro;
 	File archivo;
-	Map<Integer,Usuario> usuarios;
+	Map<Integer, Usuario> usuarios;
 	private Usuario put;
-	
+
 	public Servicio() {
 		selectorArchivos = new JFileChooser();
 		selectorArchivos.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		filtro = new FileNameExtensionFilter("Archivos txt", "txt");
 		selectorArchivos.setFileFilter(filtro);
-		usuarios = new HashMap<Integer,Usuario>();
+		usuarios = new HashMap<Integer, Usuario>();
 //		usuarios.put(1, new Usuario(1,"René Soto",26,new Date(23071993),"resotor@outlook.com"));
 //		usuarios.put(2, new Usuario(2,"Monserrat Zaragoza",24,new Date(8101993),"monserratzaragoza@outlook.com"));
 //		usuarios.put(3, new Usuario(1,"Cuau Garza",26,new Date(25031993),"resotor@outlook.com"));
 	}
-	
+
 	public boolean cargaArchivo() {
 		Usuario u;
 		boolean resultado = false;
@@ -46,46 +45,41 @@ public class Servicio {
 		String correo = "([\\w\\-]+[\\.[\\w\\-]+]*\\@[A-Za-z0-9]+[\\.[A-Za-z0-9]+]*.[A-Za-z]{2,})";
 		try {
 			Integer archResult = selectorArchivos.showOpenDialog(selectorArchivos);
-			if(archResult.equals(JFileChooser.APPROVE_OPTION)) {
+			if (archResult.equals(JFileChooser.APPROVE_OPTION)) {
 				archivo = selectorArchivos.getSelectedFile();
 				fileReader = new FileReader(archivo);
 				buffer = new BufferedReader(fileReader);
-				while((linea = buffer.readLine()) != null) {
-					Pattern patron = Pattern.compile(expresion);
+				while ((linea = buffer.readLine()) != null) {
+					Pattern patron = Pattern.compile(expresion + correo);
 					Matcher compara = patron.matcher(linea);
-					if(compara.find()) {
-						u = new Usuario(Integer.parseInt(compara.group(1)),compara.group(2),Integer.parseInt(compara.group(3)),compara.group(4),"");
-//						Tuve que separar el patron del correo ya que no lo obtiene si viene junto, aislado si lo logra,
-//						supongo que la espresion es demasiado larga
-						Pattern pt = Pattern.compile(correo);
-						Matcher mc = pt.matcher(linea);
-						if(mc.find()) u.setCorreo(mc.group());
+					if (compara.find()) {
+						u = new Usuario(Integer.parseInt(compara.group(1)),compara.group(2),Integer.parseInt(compara.group(3)),compara.group(4),compara.group(5));
 						usuarios.put(u.getId(), u);
 						System.out.println("Agrego: " + u);
-					}else {
+					} else {
 						System.out.println("No se encontro coincidencia");
 					}
 				}
 			}
 			if ((archivo == null) || (archivo.getName().equals(""))) {
-				 System.out.println("Nombre de archivo inválido");
-			}else {
+				System.out.println("Nombre de archivo inválido");
+			} else {
 				resultado = true;
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Falló carga de archivo " + e);
 			e.printStackTrace();
-		}finally {
-			
+		} finally {
+
 		}
 		return resultado;
 	}
 
 	public Usuario buscaId(int id) {
-		return usuarios.get(id);		
+		return usuarios.get(id);
 	}
-	
-	public List<Usuario> consultaUsuarios(){
+
+	public List<Usuario> consultaUsuarios() {
 		return new ArrayList(this.usuarios.values());
 	}
 }
